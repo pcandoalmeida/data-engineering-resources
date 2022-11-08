2d475df89cd7978275d134ae7b6d519aa4cec216
Asif Brohi <asifbrohi@hotmail.co.uk>
2022-11-08 12:05:55 +0000

    mini_project

---
+++ asifbrohi/mini_project/app/testing.py
@@ -0,0 +1,14 @@
+
+
+orders = {"order_1":{
+    "customer_name":"John",
+    "customer_address":"Unit 2, 12 Main Street, LONDON, WH1 2ER",
+    "customer_phone":"0789887334",
+    "status":"preparing"},
+    "order_2":{
+        "customer_name":"Ben",
+        "customer_address": "Unit 1, 34 Main Street, LONDON, W1 4ER",
+        "customer_phone":"07906753645",
+        "status":"preparing"
+    }}
+print(orders)
