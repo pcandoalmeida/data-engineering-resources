c524918be65af9a3db5f81c17b21152047848d2e
aram-de <ms_aram@outlook.com>
2022-11-14 17:25:08 +0000

    Got status update method working

--- de_lon8/aram/app4.py
+++ de_lon8/aram/app4.py
@@ -40,8 +40,6 @@ class Item():
     def check_valid_input(self, user_input : str, valid_inputs : list) -> str: 
         while user_input not in valid_inputs and user_input.capitalize() != "X" :
             print("Your choice was not valid or x to go back to the main menu.")
-            print(valid_inputs)
-            print(user_input)
             user_input = input(f"Please, choose a {self.type[:-1]} by entering the corresponding number above: ")
         if user_input.lower() == "x":
             return ""
@@ -140,7 +138,6 @@ class Item():
         item_list_as_str = ""
         while chosen_item != "d":
             chosen_item = input("Enter the number for another product you want or d if you are done: ")
-            print("These are the valid inputs:", valid_inputs)
             chosen_item = an_item.check_valid_input(chosen_item,valid_inputs)
             if chosen_item != "d":
                 item_list_as_str += chosen_item + ","
@@ -317,6 +314,51 @@ class Order(Item):
             new_status = input(f"Enter the new status: ")
             if new_status.strip() != "":
                 updatee_as_dict["status"] = new_status
+            with open(f"data\{self.type}.csv", "r") as csv_file:
+                records = csv.DictReader(csv_file, skipinitialspace=True)
+                list_of_records = (list(records))
+            list_of_records[int(updatee)-1] = updatee_as_dict
+
+            with open(f"data\{self.type}.csv", "w", newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames= self.field_names)
+                writer.writeheader()
+                for dictio in list_of_records:
+                    writer.writerow(dictio)
+            print(f"{self.type[:-1]} {updatee} has been updated")
+
+
+    def update_attributes(self) -> dict:
+        updatee_as_dict = self.asdict()
+        for key, value in updatee_as_dict.items():
+            if key == "status":
+                updatee_as_dict[key] = "Preparing"
+            elif key == "courier":
+                updatee_as_dict[key] = self.choose_courier_for_order()
+            elif key == "items":
+                updatee_as_dict[key] = self.choose_items_for_order()
+            else:
+                key_4_string = key.replace("_", " ").title()
+                newValue = input(f"Enter the {key_4_string} ")
+                while newValue == "":
+                    newValue = input("Not a valid value. Enter another: ")    
+                updatee_as_dict[key] = newValue
+        return  updatee_as_dict
+
+    def add_item_to_file(self): 
+#TODO this function does not add a line the previous line does not have a break line character
+        file_exists = os.path.isfile(f"data\{self.type}.csv")
+        #field_names = self.get_headers()
+        an_item = self.update_attributes()
+        #print("data file empty is", data_file.empty)                
+        if file_exists and os.stat(f"data\{self.type}.csv").st_size != 0: #file exists and it is not empty
+            with open(f"data\{self.type}.csv", "a", newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames= self.field_names)
+                writer.writerow(an_item)
+        else: #file either does not exist or is emtpy
+            with open(f"data\{self.type}.csv", "w", newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames= self.field_names)
+                writer.writeheader()
+                writer.writerow(an_item)
 
  
 
